d37e0d6478cb77f54a29179e69d5a4d422da7164
takusugar0 <takusugar9@gmail.com>
2020-04-25 16:37:09 +0900

    before merge

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Folder, Task
+from .forms import FolderForm, TaskForm
 
 def index(request, id):
     #すべてのフォルダを取得する
@@ -15,3 +16,30 @@ def index(request, id):
         'tasks':tasks,
         'current_folder_id': current_folder.id,
     })
+
+def create_folder(request):
+    if request.method == "POST":
+        form = FolderForm(request.POST)
+        if form.is_valid():
+            folder = form.save(commit=False)
+            folder.created_at = timezone.now()
+            folder.save()
+            return redirect('tasks.index', id=folder.id)
+    else:
+        form = FolderForm()
+    return render(request, 'create_folders.html', {'form': form})
+
+def create_task(request, id):
+    #選ばれたフォルダを取得する
+    current_folder = get_object_or_404(Folder, id=id)
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.created_at = timezone.now()
+            task.folder_id = current_folder
+            task.save()
+            return redirect('tasks.index', id=current_folder.id)
+    else:
+        form = TaskForm()
+    return render(request, 'create_tasks.html', {'form': form}, {'id':current_folder.id})
